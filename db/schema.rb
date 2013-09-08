e2654ce7e5bbf0006df5ec7e2ba3ffe6d8df662b
ArthiRengasamy <arthi.rengasamy@gmail.com>
2013-09-08 02:55:54 -0700

    updated user model and set pswd rules

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130908071617) do
+ActiveRecord::Schema.define(:version => 20130908094727) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -20,8 +20,11 @@ ActiveRecord::Schema.define(:version => 20130908071617) do
     t.text     "address"
     t.string   "area"
     t.text     "other"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
