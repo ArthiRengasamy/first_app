8a0906273790eddf493c6ad0c5074501e30e31db
ArthiRengasamy <arthi.rengasamy@gmail.com>
2013-09-15 02:41:14 -0700

    code drop updated readme

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe "User pages" do
+
+  subject { page }
+
+    describe "signup" do
+
+    before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" dd
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
+end
+
+# Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+#get static_home
+#response.status.should be(200)
